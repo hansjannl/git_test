878200b296802ca691a2011d38231e0816f4caa9
hansjannl <mail@hansjan.nl>
2023-01-24 12:14:05 +0000

    added css

--- odin-calculator/script.js
+++ odin-calculator/script.js
@@ -8,11 +8,34 @@ const key6 = document.getElementById('6');
 const key7 = document.getElementById('7');
 const key8 = document.getElementById('8');
 const key9 = document.getElementById('9');
+const keyDot = document.getElementById('dot');
 const keyClear = document.getElementById('clear');
+const keyOperate = document.getElementById('operate');
 const keyAdd = document.getElementById('add');
 const keySubtract = document.getElementById('subtract');
 const keyMultiply = document.getElementById('multiply');
 const keyDivide = document.getElementById('divide');
+const screen = document.getElementById('screen');
+const history = document.getElementById('history');
+
+key0.addEventListener("click", function () { newInput(0) });
+key1.addEventListener("click", function () { newInput(1) });
+key2.addEventListener("click", function () { newInput(2) });
+key3.addEventListener("click", function () { newInput(3) });
+key4.addEventListener("click", function () { newInput(4) });
+key5.addEventListener("click", function () { newInput(5) });
+key6.addEventListener("click", function () { newInput(6) });
+key7.addEventListener("click", function () { newInput(7) });
+key8.addEventListener("click", function () { newInput(8) });
+key9.addEventListener("click", function () { newInput(9) });
+
+keyDot.addEventListener("click", function () { newInput(".") });
+keyAdd.addEventListener("click", function () { setOperate("add") });
+keySubtract.addEventListener("click", function () { setOperate("subtract") });
+keyMultiply.addEventListener("click", function () { setOperate("multiply") });
+keyDivide.addEventListener("click", function () { setOperate("divide") });
+keyOperate.addEventListener("click", function () { operate(selectedOperator, currentInput, secondInput) });
+keyClear.addEventListener("click", function () { resetCalculation("",0) });
 
 
 //base calculator features
@@ -28,23 +51,110 @@ function multiply(first, second) {
 };
 
 function divide(first, second) {
+    if(first === 0 && second === 0){
+        return "impossible";
+    }
     return first /= second;
 };
 
 // take input and run calculator
 function operate(operator, first, second) {
+
     switch (operator) {
         case "divide":
-            divide(first, second);
+            resetCalculation(operator, divide(first, second));
             break;
         case "subtract":
-            subtract(first, second);
+            resetCalculation(operator, subtract(first, second));
             break;
         case "multiply":
-            multiply(first, second);
+            resetCalculation(operator, multiply(first, second));
             break;
         case "divide":
-            divide(first, second);
+            resetCalculation(operator, divide(first, second));
             break;
+    }
+}
+
+//take calculator input
+let currentInput = 0;
+let selectedOperator = 0;
+let secondInput = 0;
+let previousCalculation = 0;
+
+function resetCalculation(operator, result){
+    screen.innerText = result;
+    //history.innerText = currentInput + operator + secondInput;
+    currentInput = result;
+    removeOperatorActive(selectedOperator);
+
+    selectedOperator = 0;
+    secondInput = 0;
+    previousCalculation = result;
+}
+
+function setOperate(operator){
+        addOperatorActive(operator);
+        removeOperatorActive(selectedOperator);
+        selectedOperator = operator;
+}
+
+function addOperatorActive(operator){
+    switch (operator) {
+        case "add":
+            document.getElementById("add").classList.add("selected");
+            break;
+        case "subtract":
+            document.getElementById("subtract").classList.add("selected");
+            break;
+        case "multiply":
+            document.getElementById("multiply").classList.add("selected");
+            break;
+        case "divide":
+            document.getElementById("divide").classList.add("selected");
+            break;
+    }
+}
+
+function removeOperatorActive(operator){
+    switch (operator) {
+        case "add":
+            document.getElementById("add").classList.remove("selected");
+            break;
+        case "subtract":
+            document.getElementById("subtract").classList.remove("selected");
+            break;
+        case "multiply":
+            document.getElementById("multiply").classList.remove("selected");
+            break;
+        case "divide":
+            document.getElementById("divide").classList.remove("selected");
+            break;
+    }
+}
+
+function newInput(input) {
+    if (selectedOperator === 0) {
+        //only runs when no operator is set
+        if (currentInput === 0) {
+            currentInput = input;
+            screen.innerText = currentInput;
+        } else if (currentInput > 99999999) {
+        } else if (currentInput == "impossible") {
+        }else {
+            currentInput = `${currentInput}${input}`;
+            screen.innerText = currentInput;
+        }
+    } else {
+        if (secondInput === 0) {
+            secondInput = input;
+            screen.innerText = secondInput;
+            removeOperatorActive(selectedOperator);
+        } else if (secondInput > 99999999) {
+        } else {
+            secondInput = `${secondInput}${input}`;
+            screen.innerText = secondInput;
+            removeOperatorActive(selectedOperator);
+        }
     }
 }
